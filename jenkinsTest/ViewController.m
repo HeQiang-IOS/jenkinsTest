b3efc47bb1b51215a6baecb2941bc5ca357f40f5
heqiang <heqiang2011@163.com>
2020-10-25 16:41:46 +0800

    commit

--- jenkinsTest/ViewController.m
+++ jenkinsTest/ViewController.m
@@ -19,6 +19,10 @@
     UIView *v = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
     v.backgroundColor = [UIColor redColor];
     [self.view addSubview:v];
+    
+    UIView *vv = [[UIView alloc] initWithFrame:CGRectMake(100, 210, 100, 100)];
+    vv.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:vv];
 }
 
 
